a133eed05717751a5fc1bf618234ab5641a9a7a6
sritamjagadev <jagadevsritam@gmail.com>
2023-06-12 17:48:47 +0530

    sample test cases added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'i18n-sample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('i18n-sample');
+    expect(app.title).toEqual('Internationzation Sample');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('i18n-sample app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Internationzation Sample');
   });
 });
